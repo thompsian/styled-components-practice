796db685c7e65888a30367caeb61bd49bce4114f
thompsian <thompsian@gmail.com>
2021-11-02 13:00:58 -0700

    setup weekday tracker

--- src/index.js
+++ src/index.js
@@ -1,16 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Content from './components/Content'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  color: #b19cd9;
+`
+
+const Section = styled.div`
+  background-color: #ffffff;
+  border: solid 3px grey;
+  margin: 4px;
+  width: 300px;
+  height: 50px;
+  border-radius: 10px;
+  display: flex;
+  justify-content: left;
+  font-size: 40px;
+  color: #ff6961;
+`
+
+const WeekdayTitle = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 50px;
+  border-right: solid 3px lightgrey;
+`
 
 class Main extends React.Component {
   render() {
     return (
       <>
-      <h1>Hello world!</h1>
-      <Content />
+        <Title>Progress Tracker</Title>
+        <div>
+          <Section>
+            <WeekdayTitle>M</WeekdayTitle>
+          </Section>
+          <Section>
+            <WeekdayTitle>T</WeekdayTitle>
+          </Section>
+          <Section>
+            <WeekdayTitle>W</WeekdayTitle>
+          </Section>
+          <Section>
+            <WeekdayTitle>T</WeekdayTitle>
+          </Section>
+          <Section>
+            <WeekdayTitle>F</WeekdayTitle>
+          </Section>
+          <Section>
+            <WeekdayTitle>S</WeekdayTitle>
+          </Section>
+          <Section>
+            <WeekdayTitle>S</WeekdayTitle>
+          </Section>
+        </div>
       </>
     );
   }
 }
 
-ReactDOM.render(<Main />, document.getElementById('root'));
+ReactDOM.render(<Main />, document.getElementById('root'))
